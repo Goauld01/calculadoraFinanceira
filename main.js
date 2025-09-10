da19160c148c6d48950795693db4f28bb8f3d012
Fernando C Pereira <goauld@msn.com>
2025-09-10 12:03:47 -0300

    Implementada a funcionalidade do carrossel

--- main.js
+++ main.js
@@ -11,6 +11,12 @@ const form = document.getElementById("investiment-form");
 const clearButton = document.getElementById("clear-form");
 // const calculateButton = document.getElementById("calculate-results");
 
+//todo Seleção da Main
+const mainEl = document.querySelector("main");
+const carouselEl = document.getElementById("carousel");
+const nextBtn = document.getElementById("slide-arrow-next");
+const previousBtn = document.getElementById("slide-arrow-previous");
+
 //todo Variáveis globais
 let doughnutChatrReference = {};
 let progessionChartReference = {};
@@ -20,28 +26,32 @@ const columnsArray = [
   {
     columnsLabel: "Total investido",
     accessor: "investedAmount",
-    format: (numberInfo) => formatCurrency(numberInfo),
+    format: (numberInfo) => formatCurrencyToTable(numberInfo),
   },
   {
     columnsLabel: "Rendimento mensal",
     accessor: "interestReturns",
-    format: (numberInfo) => formatCurrency(numberInfo),
+    format: (numberInfo) => formatCurrencyToTable(numberInfo),
   },
   {
     columnsLabel: "Rendimento total",
     accessor: "totalInterestReturns",
-    format: (numberInfo) => formatCurrency(numberInfo),
+    format: (numberInfo) => formatCurrencyToTable(numberInfo),
   },
   {
     columnsLabel: "Quantia Total",
     accessor: "totalAmount",
-    format: (numberInfo) => formatCurrency(numberInfo),
+    format: (numberInfo) => formatCurrencyToTable(numberInfo),
   },
 ];
 
 //todo FUnção para formatar valores
-function formatCurrency(value) {
+function formatCurrencyToTable(value) {
   return value.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
+}
+
+function formatCurrencyToGraph(value) {
+  return value.toFixed(2);
 }
 
 //todo Função para calcular e exibiar os gráficos
@@ -84,66 +94,66 @@ function renderProgression(evt) {
     const finalInvestimentObject = returnsArray[returnsArray.length - 1];
 
     //todo Criação dos gráficos
-    // doughnutChatrReference = new Chart(finalMoneyChart, {
-    //   type: "doughnut",
-    //   data: {
-    //     labels: ["Total investido", "Rendimento", "Imposto"],
-    //     datasets: [
-    //       {
-    //         label: "Total",
-    //         data: [
-    //           formatCurrency(finalInvestimentObject.investedAmount),
-    //           formatCurrency(
-    //             finalInvestimentObject.totalInterestReturns *
-    //               (1 - taxRate / 100)
-    //           ),
-    //           formatCurrency(
-    //             finalInvestimentObject.totalInterestReturns * (taxRate / 100)
-    //           ),
-    //         ],
-    //         backgroundColor: [
-    //           "rgb(255, 99, 132)",
-    //           "rgb(54, 162, 235)",
-    //           "rgb(255, 205, 86)",
-    //         ],
-    //         hoverOffset: 4,
-    //       },
-    //     ],
-    //   },
-    //   options: {
-    //     responsive: true,
-    //   },
-    // });
+    doughnutChatrReference = new Chart(finalMoneyChart, {
+      type: "doughnut",
+      data: {
+        labels: ["Total investido", "Rendimento", "Imposto"],
+        datasets: [
+          {
+            label: "Total",
+            data: [
+              formatCurrencyToGraph(finalInvestimentObject.investedAmount),
+              formatCurrencyToGraph(
+                finalInvestimentObject.totalInterestReturns *
+                  (1 - taxRate / 100)
+              ),
+              formatCurrencyToGraph(
+                finalInvestimentObject.totalInterestReturns * (taxRate / 100)
+              ),
+            ],
+            backgroundColor: [
+              "rgb(255, 99, 132)",
+              "rgb(54, 162, 235)",
+              "rgb(255, 205, 86)",
+            ],
+            hoverOffset: 4,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+      },
+    });
 
-    // progessionChartReference = new Chart(progessionChart, {
-    //   type: "bar",
-    //   data: {
-    //     labels: returnsArray.map((investmentObject) => investmentObject.month),
-    //     datasets: [
-    //       {
-    //         label: "Total Investido",
-    //         data: returnsArray.map((investmentObject) =>
-    //           formatCurrency(investmentObject.investedAmount)
-    //         ),
-    //         backgroundColor: "rgb(255, 99, 132)",
-    //       },
-    //       {
-    //         label: "Retorno do Investimento",
-    //         data: returnsArray.map((investmentObject) =>
-    //           formatCurrency(investmentObject.interestReturns)
-    //         ),
-    //         backgroundColor: "rgb(54, 162, 235)",
-    //       },
-    //     ],
-    //   },
-    //   options: {
-    //     responsive: true,
-    //     scales: {
-    //       x: { stacked: true },
-    //       y: { stacked: true },
-    //     },
-    //   },
-    // });
+    progessionChartReference = new Chart(progessionChart, {
+      type: "bar",
+      data: {
+        labels: returnsArray.map((investmentObject) => investmentObject.month),
+        datasets: [
+          {
+            label: "Total Investido",
+            data: returnsArray.map((investmentObject) =>
+              formatCurrencyToGraph(investmentObject.investedAmount)
+            ),
+            backgroundColor: "rgb(255, 99, 132)",
+          },
+          {
+            label: "Retorno do Investimento",
+            data: returnsArray.map((investmentObject) =>
+              formatCurrencyToGraph(investmentObject.interestReturns)
+            ),
+            backgroundColor: "rgb(54, 162, 235)",
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        scales: {
+          x: { stacked: true },
+          y: { stacked: true },
+        },
+      },
+    });
 
     //todo Criação da tabela com os dados
     createTable(columnsArray, returnsArray, "results-table");
@@ -223,3 +233,10 @@ for (const formElement of form) {
 form.addEventListener("submit", renderProgression);
 clearButton.addEventListener("click", clearForm);
 // calculateButton.addEventListener("click", renderProgression);
+
+nextBtn.addEventListener("click", () => {
+  carouselEl.scrollLeft += mainEl.clientWidth;
+});
+previousBtn.addEventListener("click", () => {
+  carouselEl.scrollLeft -= mainEl.clientWidth;
+});
